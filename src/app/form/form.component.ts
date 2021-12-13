bff35eb6cbc652abe7c56e441dca87bedea52293
JCBackendDev <jcbackenddev@gmail.com>
2021-12-13 16:54:34 +0100

    Corrected compile errors
    The app is working all well, what a beautiful day!

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  nightMode = true;
+  logo = "🌙";
+  textColor = "text-gray-900";
+  surfaceColor = "bg-gray-900";
 
   constructor() { }
-
+  
   ngOnInit(): void {
   }
-
+  
+  changeMode() {
+    if (this.nightMode === true) {
+      this.nightMode = !this.nightMode;
+      this.logo = "☀️";
+      this.textColor = "text-gray-100";
+      this.surfaceColor = "bg-white";
+    } else {
+      this.nightMode = !this.nightMode;
+      this.logo = "🌙";
+      this.textColor = "text-gray-900";
+      this.surfaceColor = "bg-gray-900";
+    }
+  };
 }
