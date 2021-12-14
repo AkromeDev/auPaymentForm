3f077d77012bcf6878931112b1ee5ef49351e31f
JCBackendDev <jcbackenddev@gmail.com>
2021-12-14 10:44:46 +0100

    Change for the new pictures

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -11,8 +11,8 @@ export class FormComponent implements OnInit {
   textColorClass = "text-gray-100";
   surfaceColorClass = "bg-gray-900";
   imageUrl="url('../assets/16.jpg')";
-  profilImageUrl="url('../assets/1.jpg')";
-  srcUrl="../assets/1.jpg";
+  profilImageUrl="url('../assets/4.jpg')";
+  srcUrl="../assets/4.jpg";
   
   constructor() { }
   
@@ -27,7 +27,7 @@ export class FormComponent implements OnInit {
       this.surfaceColorClass = "bg-white";
       this.imageUrl = "url('../assets/19.jpg')";
       this.profilImageUrl="url('../assets/2jpg')";
-      this.srcUrl="../assets/2.jpg";
+      this.srcUrl="../assets/5.jpg";
       
     } else {
       this.nightMode = !this.nightMode;
@@ -35,8 +35,8 @@ export class FormComponent implements OnInit {
       this.textColorClass = "text-gray-100";
       this.surfaceColorClass = "bg-gray-900";
       this.imageUrl = "url('../assets/16.jpg')";
-      this.profilImageUrl="url('../assets/1.jpg')";
-      this.srcUrl="../assets/1.jpg";
+      this.profilImageUrl="url('../assets/4.jpg')";
+      this.srcUrl="../assets/4.jpg";
     }
   };
 }
