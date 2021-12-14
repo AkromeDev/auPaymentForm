ec4305635778a302da49b33370ab6a3d59586f20
JCBackendDev <jcbackenddev@gmail.com>
2021-12-14 10:44:58 +0100

    new form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { SecondFormComponent } from './second-form/second-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    SecondFormComponent
   ],
   imports: [
     BrowserModule,
