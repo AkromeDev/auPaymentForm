5d0c3de7089fe74aa1fa61cb84f11680e2972bd6
JCBackendDev <jcbackenddev@gmail.com>
2021-12-15 17:20:51 +0100

    Custom pipe Arguments added
    One new super additional feature to this pipe! adding arguments!
    This is pretty neat! thx Angular! now we can chose between cm, m and km!

--- src/app/pipes/miles-converter.pipe.ts
+++ src/app/pipes/miles-converter.pipe.ts
@@ -5,11 +5,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MilesConverterPipe implements PipeTransform {
 
-  transform(value: any, ...args: unknown[]): unknown {
+  transform(value: any, ...args: unknown[]): any {
     if (!value) {
       return '';
     }
-    return (value * 1.60934).toFixed(2);
+
+    for (let arg of args) {
+      switch(arg) {
+        case 'cm':
+          return (value * 1.60934 * 1000 * 1000).toFixed(2);
+        case 'm':
+          return (value * 1.60934 * 1000).toFixed(2);
+        case 'km':
+          return (value * 1.60934).toFixed(2);
+        default: 
+        new Error('Target unit not given or not supported');
+      }
+    };
   }
 
 }
